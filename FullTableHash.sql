6bac8c1833c3b00372932dfb9e952d91d9886a60
MazrimT <33479698+MazrimT@users.noreply.github.com>
2021-03-12 16:06:45 +0100

    Update FullTableHash.sql

--- FullTableHash.sql
+++ FullTableHash.sql
@@ -2,3 +2,10 @@ select
 	 x.*
 	,_hash = hashbytes('SHA2_512', (select x.* from (values(null))data(bar) for xml auto))
 from [databasename].[schemaname].[tableName] x
+
+								
+/* 								
+you can use the same basic idea to do a datalength on all columns at the same time
+datalength = datalength((select x.* from (values(null))data(bar) for xml auto))
+								
+*/

a7f1ace9699b0e578cc058f0fd85ead9d59ea44d
MazrimT <33479698+MazrimT@users.noreply.github.com>
2021-05-27 16:43:46 +0200

    Update FullTableHash.sql

--- FullTableHash.sql
+++ FullTableHash.sql
@@ -5,7 +5,7 @@ from [databasename].[schemaname].[tableName] x
 
 								
 /* 								
-you can use the same basic idea to do a datalength on all columns at the same time
+had an idea to also be able to use this for datalength for all columns, but problem is it addas all the keys and other things so it will show way bigger size than just the data..
 datalength = datalength((select x.* from (values(null))data(bar) for xml auto))
 								
 */
